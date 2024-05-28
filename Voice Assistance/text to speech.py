0b06f39e71ca9dd0a79f4652385f28469dd0dfeb
minnubj <126253034+minnubj@users.noreply.github.com>
2024-05-28 14:49:38 +0530

    Add files via upload

---
+++ Voice Assistance/text to speech.py
@@ -0,0 +1,17 @@
+
+#text to speech 
+import pyttsx3
+
+def say(text):
+    engine=pyttsx3.init()
+    voice=engine.getProperty('voices')
+    engine.setProperty('voice',voice[0].id)
+    engine.say(text)
+    engine.runAndWait()
+while True: 
+    text=input(print("Enter any text"))
+    say(text)
+
+
+
+    
